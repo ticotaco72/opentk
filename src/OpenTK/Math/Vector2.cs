5aa08161f92a997fb3e8e72e987c68a3482bf699
Dean Herbert <pe@ppy.sh>
2017-07-31 15:18:04 +0900

    Remove formatting fixes
    # Conflicts:
    #	src/OpenTK/Math/Vector2.cs

--- src/OpenTK/Math/Vector2.cs
+++ src/OpenTK/Math/Vector2.cs
@@ -68,10 +68,8 @@ namespace OpenTK
         /// <summary>
         /// Gets or sets the value at the index of the Vector.
         /// </summary>
-        public float this[int index]
-        {
-            get
-            {
+        public float this[int index] {
+            get{
                 if(index == 0)
                 {
                     return X;
@@ -81,9 +79,7 @@ namespace OpenTK
                     return Y;
                 }
                 throw new IndexOutOfRangeException("You tried to access this vector at index: " + index);
-            }
-            set
-            {
+            } set{
                 if(index == 0)
                 {
                     X = value;
