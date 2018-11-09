2f8b3748901b8eb7e870f78f4a0ebb64afe1629e
Shane Woolcock <samahnub@gmail.com>
2018-11-09 21:59:15 +1030

    Add Android project

---
+++ osuTK.Android/AndroidDisplayDeviceDriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using osuTK.Platform;
+
+namespace osuTK.Android
+{
+    public class AndroidDisplayDeviceDriver : IDisplayDeviceDriver
+    {
+        private static DisplayDevice dev;
+        static AndroidDisplayDeviceDriver()
+        {
+            dev = new DisplayDevice();
+            dev.IsPrimary = true;
+            dev.BitsPerPixel = 16;
+        }
+
+        public List<DisplayDevice> AvailableDevices => new List<DisplayDevice>(new DisplayDevice[] { dev });
+
+        public DisplayDevice GetDisplay(DisplayIndex displayIndex) =>
+            (displayIndex == DisplayIndex.First || displayIndex == DisplayIndex.Primary) ? dev : null;
+
+        public bool TryChangeResolution(DisplayDevice device, DisplayResolution resolution) => false;
+
+        public bool TryRestoreResolution(DisplayDevice device) => false;
+    }
+}
---
+++ osuTK.Android/AndroidGameView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Android.App;
+using osuTK.Platform;
+
+namespace osuTK.Android
+{
+    public class AndroidGameView //: Activity, IGameWindow
+    {
+        // TODO: stuff
+    }
+}
